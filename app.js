17c0fcc01eba280686e71867e63ea1b37a11a464
Alexandr Malafeev <aleksandr.malafeev@realize-it.ru>
2018-07-17 15:42:21 +0300

    add basic server

---
+++ app.js
@@ -0,0 +1,23 @@
+import TelegramBot from "node-telegram-bot-api";
+import config from "config";
+import Koa from "koa";
+import Router from "koa-router";
+
+const TOKEN = config.get("token");
+const PORT = config.get("port");
+const bot = new TelegramBot(TOKEN);
+bot.setWebHook(`${config.get("url")}/bot`);
+
+const app = new Koa();
+const router = Router();
+
+router.post("/bot", ctx => {
+	console.log(ctx);
+	ctx.status = 200;
+});
+
+app.use(router.routes());
+
+app.listen(PORT, () => {
+	console.log(`Listening on ${PORT}`);
+});
