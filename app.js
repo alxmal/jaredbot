c2cc94e89c1bfe02e56c644897c27c79c2f3cba5
Alexandr Malafeev <aleksandr.malafeev@realize-it.ru>
2018-07-19 21:31:46 +0300

    test

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 process.env["NTBA_FIX_319"] = 1;
 
-import TelegramBot from "node-telegram-bot-api";
-import config from "config";
-import Koa from "koa";
-import Router from "koa-router";
-import bodyParser from "koa-bodyparser";
-import fetch from "node-fetch";
+const TelegramBot = require("node-telegram-bot-api");
+const config = require("config");
+const Koa = require("koa");
+const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
+const fetch = require("node-fetch");
 
 const TOKEN = config.get("token");
 const PORT = config.get("port");
@@ -55,25 +55,6 @@ bot.onText(/\/about/, msg => {
 	bot.sendMessage(id, answer);
 });
 
-// /* Стикер с медведем в ответ на ругательство */
-// bot.on("message", msg => {
-// 	const {
-// 		chat: { id },
-// 		text
-// 	} = msg;
-// 	let textInclude = "блядь";
-// 	let stickerId = "CAADAgADkgEAAhmGAwABwd3g-2GZO1wC";
-// 	if (
-// 		text &&
-// 		text
-// 			.toString()
-// 			.toLowerCase()
-// 			.includes(textInclude)
-// 	) {
-// 		bot.sendSticker(id, stickerId);
-// 	}
-// });
-
 /* Стикер с Путиным в ответ на упоминание */
 bot.on("message", msg => {
 	const {
@@ -93,40 +74,6 @@ bot.on("message", msg => {
 	}
 });
 
-/* Отправить рандомную гифку с giphy api */
-// bot.on("message", msg => {
-// 	const {
-// 		chat: { id },
-// 		text
-// 	} = msg;
-// 	const giphy = {
-// 		baseURL: "https://api.giphy.com/v1/gifs/",
-// 		key: config.get("giphy_key"),
-// 		tag: "fail",
-// 		type: "random",
-// 		rating: "pg-13"
-// 	};
-// 	let gifURL = encodeURI(
-// 		giphy.baseURL + giphy.type + "?api_key=" + giphy.key
-// 	);
-// 	let textInclude = "Джаред дай гифку";
-
-// 	request(gifURL, function(error, response, body) {
-// 		if (!error && response.statusCode == 200) {
-// 			let data = JSON.parse(body);
-// 			if (
-// 				text &&
-// 				text
-// 					.toString()
-// 					.toLowerCase()
-// 					.includes(textInclude)
-// 			) {
-// 				bot.sendDocument(id, data.data.images.downsized_large.url);
-// 			}
-// 		}
-// 	});
-// });
-
 bot.onText(/\/gifme/, msg => {
 	const {
 		chat: { id },
@@ -143,12 +90,6 @@ bot.onText(/\/gifme/, msg => {
 		giphy.baseURL + giphy.type + "?api_key=" + giphy.key
 	);
 
-	// request(gifURL, function(error, response, body) {
-	// 	if (!error && response.statusCode == 200) {
-	// 		let data = JSON.parse(body);
-	// 		bot.sendDocument(id, data.data.images.downsized_large.url);
-	// 	}
-	// });
 	async function getAsyncURL(url) {
 		try {
 			const res = await fetch(url);
