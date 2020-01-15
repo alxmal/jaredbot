8bf883c2559fff9c1fdc1d98a299d5be580109b4
Alexandr Malafeev <alxmalafeev@gmail.com>
2020-01-15 18:43:24 +0300

    update project

--- app.js
+++ app.js
@@ -1,183 +1,190 @@
-process.env["NTBA_FIX_319"] = 1;
-
-const TelegramBot = require("node-telegram-bot-api");
+require('dotenv').config()
+const Telegraf = require("telegraf")
+const express = require("express")
 const config = require("config");
-const Koa = require("koa");
-const Router = require("koa-router");
-const bodyParser = require("koa-bodyparser");
-const fetch = require("node-fetch");
 const CronJob = require("cron").CronJob;
 
-const TOKEN = config.get("token");
-const PORT = config.get("port");
-const bot = new TelegramBot(TOKEN);
-bot.setWebHook(`${config.get("url")}/bot`);
+const TOKEN = process.env.BOT_TOKEN;
+const PORT = process.env.PORT;
 
-const app = new Koa();
-const router = Router();
+const app = express();
 
-router.post("/bot", ctx => {
-	const { body } = ctx.request;
-	bot.processUpdate(body);
-	ctx.status = 200;
-});
+const bot = new Telegraf(TOKEN)
+bot.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hello</b>'))
 
-app.use(bodyParser());
-app.use(router.routes());
+// Set telegram webhook
+bot.telegram.setWebhook('https://----.localtunnel.me/secret-path')
 
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+app.use(bot.webhookCallback('/secret-path'))
 app.listen(PORT, () => {
-	console.log(`Listening on ${PORT}`);
-});
+	console.log(`Example app listening on port ${PORT}!`)
+})
+
+// router.post("/bot", ctx => {
+// 	const { body } = ctx.request;
+// 	bot.processUpdate(body);
+// 	ctx.status = 200;
+// });
+
+// app.use(bodyParser());
+// app.use(router.routes());
+
+// app.listen(PORT, () => {
+// 	console.log(`Listening on ${PORT}`);
+// });
 
 /////* Планировщик задач *////
-let morningGreetJob = new CronJob({
-	cronTime: "00 00 09 * * 1-5",
-	onTick: () => {
-		let morningGreet = "Доброе утро!";
-		let chatId = -1001095382082;
-		bot.sendMessage(chatId, morningGreet);
-	},
-	start: true,
-	timeZone: "Europe/Moscow"
-});
+// let morningGreetJob = new CronJob({
+// 	cronTime: "00 00 09 * * 1-5",
+// 	onTick: () => {
+// 		let morningGreet = "Доброе утро!";
+// 		let chatId = -1001095382082;
+// 		bot.sendMessage(chatId, morningGreet);
+// 	},
+// 	start: true,
+// 	timeZone: "Europe/Moscow"
+// });
 
-let timeToEat = new CronJob({
-	cronTime: "00 00 12 * * 1-5",
-	onTick: () => {
-		let eatMessage = "Время обеда, приятного аппетита.";
-		let chatId = -1001095382082;
-		bot.sendMessage(chatId, eatMessage);
-	},
-	start: true,
-	timeZone: "Europe/Moscow"
-});
+// let timeToEat = new CronJob({
+// 	cronTime: "00 00 12 * * 1-5",
+// 	onTick: () => {
+// 		let eatMessage = "Время обеда, приятного аппетита.";
+// 		let chatId = -1001095382082;
+// 		bot.sendMessage(chatId, eatMessage);
+// 	},
+// 	start: true,
+// 	timeZone: "Europe/Moscow"
+// });
 
-let goHome = new CronJob({
-	cronTime: "00 55 17 * * 1-5",
-	onTick: () => {
-		let byeMessage = "Пора домой!";
-		let chatId = -1001095382082;
-		bot.sendMessage(chatId, byeMessage);
-	},
-	start: true,
-	timeZone: "Europe/Moscow"
-});
+// let goHome = new CronJob({
+// 	cronTime: "00 55 17 * * 1-5",
+// 	onTick: () => {
+// 		let byeMessage = "Пора домой!";
+// 		let chatId = -1001095382082;
+// 		bot.sendMessage(chatId, byeMessage);
+// 	},
+// 	start: true,
+// 	timeZone: "Europe/Moscow"
+// });
 
-let itsFriday = new CronJob({
-	cronTime: "03 55 17 * * 5",
-	onTick: () => {
-		let bamboleilo = "БАМБОЛЕЙЛОООО, БАМБОЛЕЙЛООООО!";
-		let voiceId = "AwADAgADfAEAAl3-kUqHo5Gi9RablQI";
-		let chatId = -1001095382082;
-		bot.sendVoice(chatId, voiceId);
-		bot.sendMessage(chatId, bamboleilo);
-	},
-	start: true,
-	timeZone: "Europe/Moscow"
-});
+// let itsFriday = new CronJob({
+// 	cronTime: "03 55 17 * * 5",
+// 	onTick: () => {
+// 		let bamboleilo = "БАМБОЛЕЙЛОООО, БАМБОЛЕЙЛООООО!";
+// 		let voiceId = "AwADAgADfAEAAl3-kUqHo5Gi9RablQI";
+// 		let chatId = -1001095382082;
+// 		bot.sendVoice(chatId, voiceId);
+// 		bot.sendMessage(chatId, bamboleilo);
+// 	},
+// 	start: true,
+// 	timeZone: "Europe/Moscow"
+// });
 
 /////* Обработчики чата для бота *////
-/* Отладка */
-bot.on("message", msg => {
-	const {
-		chat: { id }
-	} = msg;
-	console.log(msg);
-});
+// /* Отладка */
+// bot.on("message", msg => {
+// 	const {
+// 		chat: { id }
+// 	} = msg;
+// 	console.log(msg);
+// });
 
-/* Ответ на /help */
-bot.onText(/\/help/, msg => {
-	const {
-		chat: { id }
-	} = msg;
-	let answer = "Джаред пока ничего не умеет 😕";
-	bot.sendMessage(id, answer);
-});
+// /* Ответ на /help */
+// bot.onText(/\/help/, msg => {
+// 	const {
+// 		chat: { id }
+// 	} = msg;
+// 	let answer = "Джаред пока ничего не умеет 😕";
+// 	bot.sendMessage(id, answer);
+// });
 
-/* Ответ на /about */
-bot.onText(/\/about/, msg => {
-	const {
-		chat: { id }
-	} = msg;
-	let answer =
-		"Джаред скрам-мастер от бога. Он поможет с любым вопросом и решит любую проблему. Просто спроси у Джареда!";
-	bot.sendMessage(id, answer);
-});
+// /* Ответ на /about */
+// bot.onText(/\/about/, msg => {
+// 	const {
+// 		chat: { id }
+// 	} = msg;
+// 	let answer =
+// 		"Джаред скрам-мастер от бога. Он поможет с любым вопросом и решит любую проблему. Просто спроси у Джареда!";
+// 	bot.sendMessage(id, answer);
+// });
 
-/* Стикер с Путиным в ответ на упоминание */
-bot.on("message", msg => {
-	const {
-		chat: { id },
-		text
-	} = msg;
-	let textInclude = "путин";
-	let stickerId = "CAADAgADdwEAAp6c1AXSYeGRV6WhyAI";
-	if (
-		text &&
-		text
-			.toString()
-			.toLowerCase()
-			.includes(textInclude)
-	) {
-		bot.sendSticker(id, stickerId);
-	}
-});
+// /* Стикер с Путиным в ответ на упоминание */
+// bot.on("message", msg => {
+// 	const {
+// 		chat: { id },
+// 		text
+// 	} = msg;
+// 	let textInclude = "путин";
+// 	let stickerId = "CAADAgADdwEAAp6c1AXSYeGRV6WhyAI";
+// 	if (
+// 		text &&
+// 		text
+// 			.toString()
+// 			.toLowerCase()
+// 			.includes(textInclude)
+// 	) {
+// 		bot.sendSticker(id, stickerId);
+// 	}
+// });
 
-bot.onText(/\/gifme/, msg => {
-	const {
-		chat: { id }
-	} = msg;
-	const text = "Какую гифку запостить?";
+// bot.onText(/\/gifme/, msg => {
+// 	const {
+// 		chat: { id }
+// 	} = msg;
+// 	const text = "Какую гифку запостить?";
 
-	let keyboardStr = JSON.stringify({
-		inline_keyboard: [
-			[
-				{ text: "😂", callback_data: "funny" },
-				{ text: "🤦‍", callback_data: "fail" },
-				{ text: "😼", callback_data: "cats" },
-				{ text: "🤔", callback_data: "memes" },
-				{ text: "🎲", callback_data: "random" }
-			]
-		]
-	});
+// 	let keyboardStr = JSON.stringify({
+// 		inline_keyboard: [
+// 			[
+// 				{ text: "😂", callback_data: "funny" },
+// 				{ text: "🤦‍", callback_data: "fail" },
+// 				{ text: "😼", callback_data: "cats" },
+// 				{ text: "🤔", callback_data: "memes" },
+// 				{ text: "🎲", callback_data: "random" }
+// 			]
+// 		]
+// 	});
 
-	let keyboard = {
-		reply_markup: JSON.parse(keyboardStr)
-	};
+// 	let keyboard = {
+// 		reply_markup: JSON.parse(keyboardStr)
+// 	};
 
-	bot.sendMessage(id, text, keyboard);
-});
+// 	bot.sendMessage(id, text, keyboard);
+// });
 
-const giphy = {
-	baseURL: "https://api.giphy.com/v1/gifs/",
-	key: config.get("giphy_key"),
-	type: "random",
-	rating: "pg-13"
-};
+// const giphy = {
+// 	baseURL: "https://api.giphy.com/v1/gifs/",
+// 	key: config.get("giphy_key"),
+// 	type: "random",
+// 	rating: "pg-13"
+// };
 
-bot.on("callback_query", msg => {
-	const chatId = msg.message.chat.id;
-	console.log(msg);
-	let gifURL = encodeURI(
-		giphy.baseURL +
-			giphy.type +
-			"?api_key=" +
-			giphy.key +
-			"&tag=" +
-			msg.data
-	);
-	getAsyncURL(gifURL, chatId);
-});
+// bot.on("callback_query", msg => {
+// 	const chatId = msg.message.chat.id;
+// 	console.log(msg);
+// 	let gifURL = encodeURI(
+// 		giphy.baseURL +
+// 			giphy.type +
+// 			"?api_key=" +
+// 			giphy.key +
+// 			"&tag=" +
+// 			msg.data
+// 	);
+// 	getAsyncURL(gifURL, chatId);
+// });
 
-async function getAsyncURL(url, chatId) {
-	try {
-		const res = await fetch(url);
-		const data = await res.json();
-		bot.sendDocument(chatId, data.data.images.downsized_large.url);
-	} catch (err) {
-		console.log(err);
-	}
-}
+// async function getAsyncURL(url, chatId) {
+// 	try {
+// 		const res = await fetch(url);
+// 		const data = await res.json();
+// 		bot.sendDocument(chatId, data.data.images.downsized_large.url);
+// 	} catch (err) {
+// 		console.log(err);
+// 	}
+// }
 
 // getAsyncURL(gifURL);
 
