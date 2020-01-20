ef640879afc7b39cab7ca58295852804fef3fa27
Alexandr Malafeev <alxmalafeev@gmail.com>
2020-01-20 18:46:17 +0300

    fix

--- bdays.js
+++ bdays.js
@@ -1,16 +1,16 @@
 const bdays = [
-	["Rustam", "@hideho471", "2000-04-06"],
-	["Дмитрий", "", "2000-06-04"],
-	["Sasha Krivchenko", "@tozhesasha", "2000-03-13"],
-	["alexy.firsov", "@virmir49", "2000-09-09"],
-	["Vitaliy Baker", "@nicatelea", "2000-01-16"],
-	["Petr Chirkov", "@plashots", "2000-06-16"],
-	["Ф Fryazino Andrew", "@andrewfryazinburg", "2000-05-25"],
-	["Sasha Nris", "@SashaNris", "2000-10-08"],
-	["Александра Боброва", "@sashabobrova", "2000-01-01"],
-	["Denis Chudinov", "@soulage23", "2000-08-23"],
-	["Alexander Petrov", "", "2000-02-26"],
-	["Dzhami", "@k_dzhami", "2000-11-10"],
-	["Alexandr Malafeev", "@alxmalafeev", "2000-11-06"]
+	["Rustam", "@hideho471", "2021-04-06"],
+	["Дмитрий", "", "2021-06-04"],
+	["Sasha Krivchenko", "@tozhesasha", "2021-03-13"],
+	["alexy.firsov", "@virmir49", "2021-09-09"],
+	["Vitaliy Baker", "@nicatelea", "2021-01-16"],
+	["Petr Chirkov", "@plashots", "2021-06-16"],
+	["Ф Fryazino Andrew", "@andrewfryazinburg", "2021-05-25"],
+	["Sasha Nris", "@SashaNris", "2021-10-08"],
+	["Александра Боброва", "@sashabobrova", "2021-01-01"],
+	["Denis Chudinov", "@soulage23", "2021-08-23"],
+	["Alexander Petrov", "", "2021-02-26"],
+	["Dzhami", "@k_dzhami", "2021-11-10"],
+	["Alexandr Malafeev", "@alxmalafeev", "2021-11-06"]
 ];
 module.exports = bdays;
