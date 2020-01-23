a00c31b7137e11c1823c7ca7b485b3097144ee6a
Alexandr Malafeev <alxmalafeev@gmail.com>
2020-01-23 13:45:44 +0300

    fix

--- jared.js
+++ jared.js
@@ -43,13 +43,13 @@ let mongoConnectString = `mongodb://127.0.0.1:27017`;
 mongoose.connect(mongoConnectString, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
-	user: "alxmal",
-	pass: "z;#<X6Q*pp(2AfP>",
+	user: "jared",
+	pass: "BSRT56PV4ftzsdFT",
 	dbName: "jareddb"
 });
 
 let db = mongoose.connection;
-db.once("open", () => console.log("Connected to the database"));
+db.once("open", () => console.log("Connected to the DB"));
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 /* Bot actions */
