22f6ac04f36eb1e7756ad658ec533249ed71ca40
Alexandr Malafeev <alxmalafeev@gmail.com>
2020-01-24 16:12:29 +0300

    fix

--- jared.js
+++ jared.js
@@ -50,7 +50,7 @@ mongoose.connect(mongoConnectString, {
 
 let db = mongoose.connection;
 db.on("connecting", () => console.log("Connecting to DB..."));
-db.once("open", () => console.log("Connected to the DB", dbName));
+db.once("open", (db) => console.log("Connected to the DB", db));
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 /* Bot actions */
