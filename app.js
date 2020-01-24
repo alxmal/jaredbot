28bd853adc149a69362c39eb77ec7e0d934671ac
Alexandr Malafeev <alxmalafeev@gmail.com>
2020-01-24 18:35:57 +0300

    add say route

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
+const router = express.Router();
 const pkg = require("./package.json");
 
 const PORT = process.env.PORT;
 const app = express();
 
 // Load bot
-const jared = require("./jared")
+const jared = require("./jared");
 
-app.use(jared)
+app.use(jared);
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
-app.get("/say", (req, res) => {
-	res.sendFile('say.html');
+router.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname + "views/say.html"));
 });
+
+app.use('/', router);
 
 app.listen(PORT, () => {
 	console.log(`Jared Bot version ${pkg.version} running on port ${PORT}!`);
