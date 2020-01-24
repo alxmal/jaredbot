368bcb95da5b9cda9309a7b140485f12d8a51fe1
Alexandr Malafeev <alxmalafeev@gmail.com>
2020-01-24 16:24:54 +0300

    fix

--- jared.js
+++ jared.js
@@ -40,9 +40,11 @@ const getClosestDateIndex = arr => {
 let mongoConnectString = `mongodb://${DBUSER}:${DBPWD}@${DBHOST}/${DBNAME}`;
 // let mongoConnectString = `mongodb://127.0.0.1:27017`;
 
+console.log(mongoConnectString);
+
 mongoose.connect(mongoConnectString, {
 	useNewUrlParser: true,
-	useUnifiedTopology: true,
+	useUnifiedTopology: true
 	// user: "jared",
 	// pass: "BSRT56PV4ftzsdFT",
 	// dbName: "jareddb"
