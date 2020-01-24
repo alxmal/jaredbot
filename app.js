57d81c3dd7a2712e667dae6f2d30e45e37cd893d
Alexandr Malafeev <alxmalafeev@gmail.com>
2020-01-24 18:33:30 +0300

    add say route

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
+app.get("/say", (req, res) => {
+	res.sendFile('say.html');
+});
+
 app.listen(PORT, () => {
 	console.log(`Jared Bot version ${pkg.version} running on port ${PORT}!`);
 });
